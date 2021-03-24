f065235643061e9ada22cf367ff3ef6249ee5e66
gearnode <bryan@frimin.fr>
2021-03-24 11:54:09 +0100

    Add totp validation functions

--- src/totp.erl
+++ src/totp.erl
@@ -15,7 +15,8 @@
 -module(totp).
 
 -export([generate/1, generate/2, generate/3,
-         new_validator/1, new_validator/2]).
+         new_validator/1, new_validator/2,
+         validate/2, validate/3]).
 
 -export_type([key/0, password/0, password_size/0,
               timestamp/0, step/0,
@@ -86,3 +87,36 @@ new_validator(Key, Options) ->
     initial_time => maps:get(initial_time, Options, 0),
     look_behind => maps:get(look_behind, Options, 1),
     look_ahead => maps:get(look_ahead, Options, 1)}.
+
+-spec validate(validator_state(), password()) ->
+        {valid, validator_state()} | invalid.
+validate(State, Password) ->
+  validate(State, Password, os:system_time(second)).
+
+-spec validate(validator_state(), password(), timestamp()) ->
+        {valid, validator_state()} | invalid.
+validate(#{initial_time := InitialTime, step := Step, size := Size,
+           look_behind := LookBehind, look_ahead := LookAhead,
+           algorithm := Algorithm, key := Key} = State,
+         Password, CurrentTime) ->
+  LastPeriod = maps:get(last_period, State, none),
+  TimePeriod = time_period(CurrentTime, InitialTime, Step),
+  if
+    LastPeriod =:= TimePeriod ->
+      invalid;
+    true ->
+      PossiblePeriods =
+        lists:seq(TimePeriod - LookBehind, TimePeriod + LookAhead),
+      IsValidPassword =
+        fun(Period) ->
+            hotp:generate(Key, Period, #{size => Size, algorithm => Algorithm})
+              =:= Password
+        end,
+      case lists:search(IsValidPassword, PossiblePeriods) of
+        false ->
+          invalid;
+        {value, NewLastPeriod} ->
+          State1 = State#{last_period => NewLastPeriod},
+          {valid, State1}
+      end
+  end.
